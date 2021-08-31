553cc4dd701c932b042df4960f2a61b4db1db9a1
William Spiro <35824747+williamspiro@users.noreply.github.com>
2021-08-31 17:16:15 -0400

    Update index with some info

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 
 export default ({ data }) => (
   <div>
-    <h1>My properties</h1>
+    <h1>Gatsby + Netlify site with dynamic routes and CRM data from collector/v3/graphql GraphQL API</h1>
     {data.hubs.CRM.p_propertylisting_collection.items.map((item, i) => (
       <Link key={i} to={item.dynamic_page_slug_3}>
         <h2>
