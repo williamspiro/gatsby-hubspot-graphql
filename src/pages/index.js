7b77133cbd8d61db273365b455c023e00b39e557
William Spiro <35824747+williamspiro@users.noreply.github.com>
2021-10-13 13:15:30 -0400

    Note HubSpot

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 
 export default ({ data }) => (
   <div>
-    <h1>Gatsby + Netlify site with dynamic routes and CRM data from collector/v3/graphql GraphQL API</h1>
+    <h1>Gatsby + Netlify site with dynamic routes and CRM custom object data from HubSpot collector/v3/graphql GraphQL API</h1>
     {data.hubs.CRM.p_propertylisting_collection.items.map((item, i) => (
       <Link key={i} to={item.dynamic_page_slug_3}>
         <h2>
