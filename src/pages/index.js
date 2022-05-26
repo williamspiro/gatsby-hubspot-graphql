cc9c3be01dd0f120d48689ae6d9427df9a10bc86
William Spiro <35824747+williamspiro@users.noreply.github.com>
2022-05-26 17:11:30 -0400

    Move to products

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import React from "react"
 
 export default ({ data }) => (
   <div>
-    <h1>Gatsby + Netlify site with dynamic routes and CRM custom object data from HubSpot collector/v3/graphql GraphQL API</h1>
-    {data.hubs.CRM.p_propertylisting_collection.items.map((item, i) => (
-      <Link key={i} to={item.dynamic_page_slug_3}>
+    <h1>Gatsby + Netlify site with dynamic routes and CRM custom object from HubSpot collector/v3/graphql GraphQL API</h1>
+    {data.hubs.CRM.product_collection.items.map((item, i) => (
+      <Link key={i} to={item.product_slug}>
         <h2>
-          {item.address}
+          {item.name}
         </h2>
       </Link>
     ))}
@@ -18,10 +18,13 @@ export const query = graphql`
   query {
     hubs {
       CRM {
-        p_propertylisting_collection {
+        product_collection {
           items {
-            dynamic_page_slug_3
-            address
+            name
+            product_slug
+            description
+            price
+            product_image
           }
         }
       }
