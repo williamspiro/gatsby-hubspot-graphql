b78145b68129c2672add3102e87cca0abd815a77
William Spiro <35824747+williamspiro@users.noreply.github.com>
2022-05-26 17:19:32 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 
 export default ({ data }) => (
   <div>
-    <h1>Gatsby + Netlify site with dynamic routes and CRM object from HubSpot <a href"https://developers.hubspot.com/docs/cms/data/query-hubspot-data-using-graphql">collector/v3/graphql GraphQL API</a></h1>
+    <h1>Gatsby + Netlify site with dynamic routes and CRM object from HubSpot <a href="https://developers.hubspot.com/docs/cms/data/query-hubspot-data-using-graphql">collector/v3/graphql GraphQL API</a></h1>
     {data.hubs.CRM.product_collection.items.map((item, i) => (
       <Link key={i} to={item.product_slug}>
         <h2>
